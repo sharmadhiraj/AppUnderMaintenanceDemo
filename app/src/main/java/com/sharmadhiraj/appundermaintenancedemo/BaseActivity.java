c9c47097e19dbea9a1d5d3fd13e5e0f483cc4c9f
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2020-05-05 09:20:54 +0545

    Migrate project to AndroidX.

--- app/src/main/java/com/sharmadhiraj/appundermaintenancedemo/BaseActivity.java
+++ app/src/main/java/com/sharmadhiraj/appundermaintenancedemo/BaseActivity.java
@@ -2,10 +2,11 @@ package com.sharmadhiraj.appundermaintenancedemo;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
